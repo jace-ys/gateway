4461f94666840e6c97873caa487ed5f4fb752751
Joan Tso <joantsomw@yahoo.com.hk>
2018-11-11 01:32:35 +0000

    fix conflict in QueueChart

--- client/src/QueueChart.js
+++ client/src/QueueChart.js
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
-import React, {
-  Component
-} from 'react';
-import {
-  Bar
-} from 'react-chartjs-2';
-import {
-  chart
-} from './chartOptions';
-=======
 import React, { Component } from 'react';
 import { Bar } from 'react-chartjs-2';
 import socketIOClient from 'socket.io-client';
 import { chart } from './chartOptions';
->>>>>>> 72f0f77b56de623b31045322d3ffa464473bbab2
 
 export default class QueueChart extends Component {
   constructor() {
     super();
     this.state = {
-<<<<<<< HEAD
       queue_status : [0,0,0,0]
     };
   }
@@ -47,41 +34,16 @@ export default class QueueChart extends Component {
       labels: ['Gate 1', 'Gate 2', 'Gate 3', 'Gate 4', 'Gate 5', 'Gate 6', 'Gate 7', 'Gate 8'],
       datasets: [{
           label: 'Groups w/ children',
-=======
-      queues: [30, 48, 20, 38, 42, 25, 34, 24]
-    };
-  }
-
-  componentDidMount() {
-    const socket = socketIOClient('http://localhost:3000');
-    socket.on('newCheckIn', (queueData) => {
-      this.setState({ queues: queueData });
-    });
-  }
-
-  render() {
-    const { queues } =  this.state;
-
-    const plotData = {
-      labels: ['Gate 1', 'Gate 2', 'Gate 3', 'Gate 4', 'Gate 5', 'Gate 6', 'Gate 7', 'Gate 8'],
-      datasets: [
-        {
->>>>>>> 72f0f77b56de623b31045322d3ffa464473bbab2
           backgroundColor: 'rgba(255,99,132,0.4)',
           borderColor: 'rgba(255,99,132,1)',
           borderWidth: 1,
           hoverBackgroundColor: 'rgba(255,99,132,0.6)',
           hoverBorderColor: 'rgba(255,99,132,1)',
-<<<<<<< HEAD
           data: queue_status
-=======
-          data: queues
->>>>>>> 72f0f77b56de623b31045322d3ffa464473bbab2
         }
       ],
     };
 
-<<<<<<< HEAD
     return ( <
       div >
       <
@@ -101,17 +63,4 @@ export default class QueueChart extends Component {
       /div>
     );
   }
-=======
-    return (
-  		<div>
-        <Bar
-        	data={plotData}
-        	width={400}
-        	height={450}
-        	options={chart.options}
-        />
-  		</div>
-		);
-	}
->>>>>>> 72f0f77b56de623b31045322d3ffa464473bbab2
 }
